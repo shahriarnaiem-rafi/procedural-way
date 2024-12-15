3069432e9f0cba627132805d8c5aa5f2ad7ed417
shahriarnaiem-rafi <shahriarnaiemrafi33@gmail.com>
2024-12-15 19:04:56 +0600

    added delete trigger

--- post.php
+++ post.php
@@ -1,31 +1,39 @@
-
 <?php
 include_once "index.php";
-if(isset($_POST['product'])){
-    $pname=$_POST['pname'];
-    $price=$_POST['price'];
-    $bid=$_POST['bid'];
+if (isset($_POST['product'])) {
+    $pname = $_POST['pname'];
+    $price = $_POST['price'];
+    $bid = $_POST['bid'];
 
     $db->query("CALL product_p('$pname','$price','$bid')");
-    
+
     header("location:$_SERVER[PHP_SELF]");
 }
-if(isset($_POST["view"])){
+if (isset($_POST["view"])) {
+
     header("location:view.php");
+
 }
 
+if (isset($_POST['submitted'])) {
+    $don = $_POST['hold'];
+    $db->query("delete from product where id=$don");
+
+}
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ANother</title>
 </head>
+
 <body>
-    
-<form action="" method="post">
+
+    <form action="" method="post">
         Product Name <br>
         <input type="text" name="pname"><br>
         Price <br>
@@ -34,23 +42,33 @@ if(isset($_POST["view"])){
         Brand Name
         <select name="bid" id="">
             <?php
-            $connect=$db->query("select * from brand");
-            while(list($id,$bname)= $connect->fetch_row()){
-                echo"<option value='$id'>$bname</option>";
+            $connect = $db->query("select * from brand");
+            while (list($id, $bname) = $connect->fetch_row()) {
+                echo "<option value='$id'>$bname</option>";
             }
-                
-            
-                
             ?>
         </select>
 
-        
+
         <br><input type="submit" name="product" value="Submit">
         <br><input type="submit" name="view" value="View">
-        
+
 
     </form><br><br>
-   
-    
+    <form action="" method="post">
+        Brand Name
+        <select name="hold" id="">
+            <?php
+            $connect = $db->query("select * from brand");
+            while (list($id, $bname) = $connect->fetch_row()) {
+                echo "<option value='$id'>$bname</option>";
+            }
+            ?>
+        </select>
+        <input type="submit" value="Delete" name="submitted">
+    </form>
+
+
 </body>
+
 </html>
--- view.php
+++ view.php
@@ -1,10 +1,11 @@
-<?php 
+<?php
 include_once "index.php";
 include_once "post.php";
 
-        $show=$db->query("select * from product_details");
-     
-        echo "<table border='1' style='border-collapse:collapse; width:400px; margin-top:-300px; margin-left:300px; '>  <tr>
+
+$show = $db->query("select * from product_details");
+
+echo "<table border='1' style='border-collapse:collapse; width:400px; margin-top:-300px; margin-left:300px; '>  <tr>
 
             <th>ID</th>
             <th>Brand Name</th>
@@ -12,25 +13,21 @@ include_once "post.php";
             <th>Product Name</th>
             <th>Price</th>
             <th>Brand id</th>
+            <th>Action</th>
             
         
         
         </tr>";
-        while(list($id,$bname,$contact,$pname,$price,$bid)= $show->fetch_row()){
-            echo  "<tr>
+while (list($id, $bname, $contact, $pname, $price, $bid) = $show->fetch_row()) {
+    echo "<tr>
                 <td>$id</td>
                 <td>$bname</td>
                 <td>$contact</td>
                 <td>$pname</td>
                 <td>$price</td>
                 <td>$bid</td>
-               
-
             </tr>";
-            
-        }
-         // <td>
-        // <a href='delete.php?deleteid='$id''>Delete</a>
-        // </td>
-        echo "</table>";
- ?>
+
+}
+echo "</table>";
+?>
